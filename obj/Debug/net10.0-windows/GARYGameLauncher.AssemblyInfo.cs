e36e42e162509bbd5cd878902e56cfacce899784
Darith <phoexlow@gmail.com>
2026-08-22 02:39:02 +0700

    setup

--- obj/Debug/net10.0-windows/GARYGameLauncher.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/GARYGameLauncher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GARYGameLauncher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8659360476d255b068c47309a50386489e9d59e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GARYGameLauncher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GARYGameLauncher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
