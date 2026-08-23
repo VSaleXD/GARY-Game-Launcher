8a07409ac1e06730c1cf1546383e96d37e279193
VSaleXD <aleandremoulidouane@apps.ipb.ac.id>
2026-08-23 07:37:39 +0700

    tambah gitignore

--- obj/Debug/net10.0-windows/GARYGameLauncher.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/GARYGameLauncher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GARYGameLauncher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb112da70833b2499972200d539e519af3730ab3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca603618099968474dcf480b6a1487ea65fc543e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GARYGameLauncher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GARYGameLauncher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
